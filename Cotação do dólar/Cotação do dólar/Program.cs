7984be046489488d228877d5dff4e6152b2f077d
Dener da Silva Cardoso <dener.cardoso@alunos.sc.senac.br>
2022-05-29 06:29:16 -0300

    Enviando sistema de conversão

---
+++ Cotação do dólar/Cotação do dólar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cotação_do_dólar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
